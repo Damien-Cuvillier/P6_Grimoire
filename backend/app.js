d44e434372cd1f7d1e351f86cca4c1118481f812
Damien Cuvillier <damien.cuvillier.37@gmail.com>
2024-10-15 14:40:12 +0200

    ajout du router stuff.js et trasnfert des routes de app.js vers stuff.js

--- backend/app.js
+++ backend/app.js
@@ -22,35 +22,5 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('./api/stuff', (req, res, next )=>{
-    delete req.body._id;
-    const book=new Book ({
-        ...req.body
-    })
-    book.save()
-        .then(() => res.status(201).json({message: 'Livre enregistré'}))
-        .catch(error => res.status(400).json({error}))
-})
-app.put('/api/stuff/:id', (req, res, next)=>{
-    Book.updateOne({_id: req.params.id}, {...req.body, _id:req.params.id})
-        .then(()=>res.status(200).json({message: 'Livre modifié '}))
-        .catch(error => res.status(400).json({error}))
-})
-addEventListener.delete('/api/stuff/:id', (req, res, next)=>{
-    Book.deleteOne({ _id: req.params.id})
-        .then(()=>res.status(200).json({message:'Livre supprimé '}))
-        .catch(error => res.status(400).json({error}))
-})
-app.get('/api/stuff/:id', (req,res,next)=>{
-    Book.findOne({_id: req.params.id})
-        .then(book => res.status(200).json(book))
-        .catch(error => res.status(404).json({error}))
-})
-
-app.get('/api/stuff', (req, res, next) => {
-    Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({error}))
-  });
 
 module.exports = app;
